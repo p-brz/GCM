58ec93b8827a1cbbda32d5ea15a99968e0f98c56
souzabrizolara <souzabrizolara@gmail.com>
2015-03-23 08:54:11 -0300

    Adicionando model e controller "account".

---
+++ api/models/Account.js
@@ -0,0 +1,20 @@
+/**
+* Account.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      username: {
+        type: 'STRING',
+        defaultsTo: ''
+      },
+      balance : {
+        type: 'float',
+        defaultsTo: 0.0
+      }
+  }
+};
