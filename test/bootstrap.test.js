c580a2dcbd1926a29a23ca0a7dd3da2a0818ae9e
Alison Bento <alisonbnt@gmail.com>
2015-06-18 13:43:20 -0300

    Outra tentativa para a issue #13

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -9,14 +9,22 @@ before(function(done) {
     Sails.lift({
         // configuration for testing purposes
     }, function(err, server) {
+        console.log('Callback [Sails.lift]');
+        console.log(err);
         sails = server;
         if (err) return done(err);
         // here you can load fixtures, etc.
+        console.log('[Before Done] Callback [Sails.lift]');
         done(err, sails);
+        console.log('[After Done] Callback [Sails.lift]');
     });
 });
 
-after(function(done) {
-    // here you can clear fixtures, etc.
-    sails.lower(done);
+//after(function(done) {
+//    // here you can clear fixtures, etc.
+//    sails.lower(done);
+//});
+
+after(function(done){
+    done();
 });
---
+++ test/hello_test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alisonbnt on 18/06/15.
+ */
+
+
+
+var assert = require('chai').assert;
+describe('Hello', function(){
+    describe('#test()', function(){
+        it('should return true', function(done){
+            assert.equal(1, 1);
+            done();
+        })
+    })
+});
